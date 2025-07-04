fd014b10d738a628fc510fd04bad0b1934217f5a
Pedro Lucas  Reis de Oliveira Sousa <128660097+PedroReoli@users.noreply.github.com>
2025-07-04 14:06:47 -0300

    Rename Home component to AboutMe
    Renamed Home.tsx to AboutMe.tsx and updated the import in App.tsx to reflect the new component name. This improves clarity and better represents the component's purpose.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import StarWarsOpening from './components/StarWarsOpening'
-import Home from './components/Home'
+import Home from './components/AboutMe'
 
 const App: React.FC = () => {
   return (
